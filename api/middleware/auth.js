6016f2ea53c0f4c3ac5e09d820a6504b3e550e64
Manmit Pal <manmitpal348@gmail.com>
2025-12-07 12:25:27 +0530

    Created AUth middlewarw and user get method

---
+++ api/middleware/auth.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if(!token){
+            return res.status(401).json({
+                success:false, 
+                message:"Not Authorized - No token Provided"
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded){
+            return res.status(401).json({
+                success: false,
+                message: "Not Authorized - Invalid Token"
+            });
+        }
+
+        const currentUser = await User.findById(decoded.id);
+
+        req.user = currentUser;
+        next();
+    } catch (error) {
+        console.log("Error in auth middleware: ", error);
+        return res.status(500).json({
+            success: false,
+            message: "Not Authorized"
+        })
+    }
+}
