7417918464c73030bbfbcd0836e625f2660339ef
Manmit Pal <manmitpal348@gmail.com>
2025-12-27 12:17:44 +0530

    Created Header Component

--- client/src/Pages/ProfilePage.jsx
+++ client/src/Pages/ProfilePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../components/Header';
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='min-h-screen bg-gray-50 flex flex-col' >
+      <Header/>
+    </div>
   )
 }
 
-export default ProfilePage
+export default ProfilePage;
