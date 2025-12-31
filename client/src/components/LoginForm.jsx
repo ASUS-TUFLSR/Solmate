b7a4c8c05b840c7566eab771f138b60df415cac1
Manmit Pal <manmitpal348@gmail.com>
2025-12-31 21:09:55 +0530

    minor change

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -27,7 +27,7 @@ const LoginForm = () => {
             required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm
+            className='appearance-none text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm
             placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:border-pink-500 sm:text-sm'
             />
          </div>
@@ -46,7 +46,7 @@ const LoginForm = () => {
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm
+            className='appearance-none text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm
             placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:border-pink-500 sm:text-sm'
             />
          </div>
--- client/src/store/useMatchStore.js
+++ client/src/store/useMatchStore.js
@@ -38,7 +38,7 @@ export const useMatchStore = create((set) => ({
     swipeLeft: async (user) => {
         try {
             set({swipeFeedback: "passed"});
-            await axiosInstance.post("/matches/swipe-left" + user._id);
+            await axiosInstance.post("/matches/swipe-left/" + user._id);
         } catch (error) {
             console.log(error)
             toast.error("Failed To Swipe Left");
@@ -50,7 +50,7 @@ export const useMatchStore = create((set) => ({
     swipeRight: async (user) => {
         try {
             set({swipeFeedback: "liked"});
-            await axiosInstance.post("/matches/swipe-right" + user._id);
+            await axiosInstance.post("/matches/swipe-right/" + user._id);
         } catch (error) {
             console.log(error)
             toast.error("Failed To Swipe Right");
