be9f01136f68d31c1fb1261fbdeadb4717fd2782
Manmit Pal <manmitpal348@gmail.com>
2026-01-05 19:49:35 +0530

    Chatpage skeleton created

--- client/src/Pages/ChatPage.jsx
+++ client/src/Pages/ChatPage.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Header from "../components/Header"
+import {useMessageStore} from "../store/useMessageStore"
+import { useAuthStore } from '../store/useAuthStore';
 
 const ChatPage = () => {
+  const {messages} = useMessageStore();
+  const {authUser} = useAuthStore();
   return (
-    <div>ChatPage</div>
+    <div className='flex flex-col h-screen bg-gray-100 bg-opacity-50' >
+      <Header/>
+
+      <div className='grow flex flex-col p-4 md:p-6 lg:p-8 overflow-hidden max-w-4xl mx-auto w-full' >
+       <div className='flex items-center mb-4 bg-white rounded-lg shadow p-3' >
+           <img 
+                 src={"/avatar.png"}
+                 className='w-12 h-12 object-cover rounded-full mr-3 border-2 border-pink-300'
+           />
+           <h2 className='text-xl font-semibold text-gray-800' >Trunks</h2>
+       </div>
+
+       <div className='grow overflow-y-auto mb-4 bg-white rounded-lg shadow p-4' >
+             {messages.length === 0 ? (
+              <p className='text-center text-gray-500 py-8'>Start your conversations with john doe</p>
+             ) : (
+              messages.map((msg) => (
+                <div key={msg._id}
+                     className={`mb-3 ${msg.sender === authUser._id ? "text-right" : "text-left"}`}
+                >
+                  <span 
+                  className={`inline-block p-3 rounded-lg max-w-xs lg:max-w-md ${
+                    msg.sender === authUser._id ? "bg-pink-500 text-white" : "bg-gray-200 text-gray-800"
+                  }`}
+                  >
+                    {msg.content}
+                  </span>
+                </div>
+              ))
+             )}
+       </div>
+
+       <div>Message Input</div>
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/components/NoMoreProfiles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoMoreProfiles = () => {
+  return (
+    <div>NoMoreProfiles</div>
+  )
+}
+
+export default NoMoreProfiles
